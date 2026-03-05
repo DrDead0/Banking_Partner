d4f793de6b3f5a5a4055040615fd3d922b2726b0
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-03-05 15:31:31 +0530

    Added more auth options

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -1,17 +1,24 @@
-const userModel = require("../../models/user.model.js")
-
+const userModel = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
 
 const userRegistercontroller = async (req, res) => {
 
     //data fetching from body
-    const { email, name, password } = req.body
-    if (!email || !name || !password){
+    if (!req.body) {
         return res.status(400).json({
-            message:"All Fields are required",
+            message: "Request body is empty or missing",
             status: "failed"
         })
     }
-    
+
+    const { email, name, password } = req.body
+    if (!email || !name || !password) {
+        return res.status(400).json({
+            message: "All Fields are required",
+            status: "failed"
+        })
+    }
+
 
     // User Existance Checking
     const isUserExist = await userModel.findOne({ email })
@@ -29,6 +36,14 @@ const userRegistercontroller = async (req, res) => {
         password
     })
 
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+    res.cookies("token", token);
+    res.status(201).json(
+        {
+            message: "User Registered Successfully",
+            status: "success"
+        }
+    )
 }
 
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../config/Controllers/auth.controller.js")
+const authController = require("../Controllers/auth.controller.js")
 
 router.post("/register", authController.userRegistercontroller)
 
