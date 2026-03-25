8ec9f3d7f311a114ec03cbad36e646c9418686a9
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-03-26 01:22:19 +0530

    Added more transaction controller

--- src/Controllers/transaction.controller.js
+++ src/Controllers/transaction.controller.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose")
 async function createTransactionController(req,res){
     const {fromAccount,toAccount,amount,idempotencyKey} = req.body;
 
-    if(!fromAccount || ! toAccount || !amount || !idempotencyKey){
+    if(!fromAccount || !toAccount || !amount || !idempotencyKey){
         return res.status(400).json({
             message:"All Fields are required",
             status:"Failed"
@@ -22,11 +22,43 @@ async function createTransactionController(req,res){
    const toUserAccount = await accountModel.findById({_id:toAccount})
 
    if(!fromUserAccount||!toUserAccount){
-    return res.status(404).json({
+    return res.status(400 ).json({
         message:"Account Not Found",
         status:"Failed"
     })
    }
+   //transaction validation
+   
+   const isTransactionExist = await transactionModel.findOne({idempotencyKey:idempotencyKey})
 
+   //now check if the idempotency key already exist or not
+   //if does exist that means we are doing the same transaction again which is bad
+
+  if(isTransactionExist){
+    if(isTransactionExist.status==='COMPLETED'){
+        res.status(200).json({
+            message:"Transaction Completed",
+            status:"Success"
+        })
+    }
+    if(isTransactionExist.status==='PENDING'){
+        return res.status(400).json({
+            message:"Transaction is Pending",
+            status:"Failed"
+        })
+    }
+    if(isTransactionExist.status==='FAILED'){
+        return res.status(400).json({
+            message:"Transaction is Failed",
+            status:"Failed"
+        })
+    }
+    if(isTransactionExist.status==='REVERSED'){
+        return res.status(400).json({
+            message:"Transaction is Reversed",
+            status:"Failed"
+        })
+    }
+  }
     
 }
